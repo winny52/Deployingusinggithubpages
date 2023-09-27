9fcb8ebe220fb4470ca72563ff4f9fa4f5e44917
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-27 09:51:36 +0300

    Deploying

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BookDetails = ({ books, selectedBook }) => {
-  const filteredBooks = books.filter((book) => book.id === selectedBook);
+  // const filteredBooks = books.filter((book) => book.id === selectedBook);
 
   return (
     <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
